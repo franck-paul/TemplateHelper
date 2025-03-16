e8bb39eabe83e041531c3503d81b166d06369379
franck <carnet.franck.paul@gmail.com>
2025-03-16 10:35:23 +0100

    Be more specific on error message (exception or not): add the name of the given method

--- src/Code.php
+++ src/Code.php
@@ -51,7 +51,7 @@ class Code
                     $class = $reflection_function->getClosureScopeClass();
                     if ($class === null) {
                         if (App::config()->debugMode() || App::config()->devMode()) {
-                            throw new TemplateException('Error processing the template code (unable to get class of given method)');
+                            throw new TemplateException('Error processing the template code for ' . self::callableName($method) . ' (unable to get class of given method)');
                         }
 
                         return '';
@@ -65,7 +65,7 @@ class Code
             $filename = $reflection_method->getFileName();
             if ($filename === false) {
                 if (App::config()->debugMode() || App::config()->devMode()) {
-                    throw new TemplateException('Error processing the template code (unable to get source file)');
+                    throw new TemplateException('Error processing the template code for ' . self::callableName($method) . ' (unable to get source file)');
                 }
 
                 return '';
@@ -76,7 +76,7 @@ class Code
 
             if ($start_line === -1 || $end_line === false || ($end_line - $start_line) <= 0) {
                 if (App::config()->debugMode() || App::config()->devMode()) {
-                    throw new TemplateException('Error processing the template code (unable to get source file lines range)');
+                    throw new TemplateException('Error processing the template code for ' . self::callableName($method) . ' (unable to get source file lines range)');
                 }
 
                 return '';
@@ -85,7 +85,7 @@ class Code
             $source = file($filename);
             if ($source === false) {
                 if (App::config()->debugMode() || App::config()->devMode()) {
-                    throw new TemplateException('Error processing the template code (unable to read source file)');
+                    throw new TemplateException('Error processing the template code for ' . self::callableName($method) . ' (unable to read source file)');
                 }
 
                 return '';
@@ -105,7 +105,7 @@ class Code
                 $parameters = $reflection_method->getParameters();
                 if (count($parameters) > count($variables)) {
                     if (App::config()->debugMode() || App::config()->devMode()) {
-                        throw new TemplateException('Error processing the template code (not enough values given)');
+                        throw new TemplateException('Error processing the template code for ' . self::callableName($method) . ' (not enough values given)');
                     }
 
                     return '';
@@ -136,7 +136,7 @@ class Code
                     $body = preg_replace($preg_patterns, $preg_values, $body);
                     if ($body === null) {
                         if (App::config()->debugMode() || App::config()->devMode()) {
-                            throw new TemplateException('Error processing the template code (unable to replace variables)');
+                            throw new TemplateException('Error processing the template code for ' . self::callableName($method) . ' (unable to replace variables)');
                         }
 
                         return '';
@@ -146,18 +146,58 @@ class Code
                 $code = trim($body);
             } else {
                 if (App::config()->debugMode() || App::config()->devMode()) {
-                    throw new TemplateException('Error processing the template code (unable to get method core code)');
+                    throw new TemplateException('Error processing the template code for ' . self::callableName($method) . ' (unable to get method core code)');
                 }
 
                 return '';
             }
         } catch (Exception|TemplateException $e) {
             if (App::config()->debugMode() || App::config()->devMode()) {
-                throw new TemplateException($e->getMessage() ?: 'Error processing template code', (int) $e->getCode(), $e);
+                throw new TemplateException($e->getMessage() ?: 'Error processing template code for ' . self::callableName($method), (int) $e->getCode(), $e);
             }
-            $code = '/* Error processing method template code */';
+            $code = '/* Error processing method template code for ' . self::callableName($method) . ' */';
         }
 
         return $code;
+    }
+
+    /**
+     * Return a string representation of a callable (usually callback)
+     *
+     * @param      mixed   $callable  The callable
+     */
+    protected static function callableName(mixed $callable): string
+    {
+        $name = '(unknown)';
+
+        try {
+            if (is_string($callable)) {
+                // Simple function name (no namespace)
+                $name = $callable;
+            } elseif (is_array($callable)) {
+                // Class, method
+                $name = is_object($callable[0]) ? $callable[0]::class . '-&gt;' . $callable[1] : $callable[0] . '::' . $callable[1];
+            } elseif ($callable instanceof \Closure) {
+                // Closure
+                $r  = new ReflectionFunction($callable);
+                $ns = (bool) $r->getNamespaceName() ? $r->getNamespaceName() . '::' : '';
+                $fn = $r->getShortName() ?: '__closure__';
+                if ($ns === '') {
+                    // Cope with class::method(...) forms
+                    $c = $r->getClosureScopeClass();
+                    if (!is_null($c)) {
+                        $ns = $c->getName() !== '' ? $c->getName() . '::' : ''; // @phpstan-ignore-line
+                    }
+                }
+
+                $name = $ns . $fn;
+            } else {
+                // Not yet managed, give simpler response
+                $name = print_r($callable, true);
+            }
+        } catch (Exception) {
+        }
+
+        return $name . '()';
     }
 }
