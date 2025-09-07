e7c254c156c9e6291ce28602188f7d6869d4b548
franck <carnet.franck.paul@gmail.com>
2025-09-07 15:44:47 +0200

    New release 2.0

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Template Helper',
     'Template Helper methods',
     'Franck Paul',
-    '1.8',
+    '2.0',
     [
-        'date'        => '2025-03-15T23:44:00+01.8',
+        'date'        => '2025-03-15T23:44:00+02.0',
         'requires'    => [['core', '2.36']],
         'type'        => 'plugin',
         'permissions' => 'My',
