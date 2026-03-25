73c46d3b55f63ed96c2b52455b8e82603fc5e6cd
franck <contact@open-time.net>
2026-03-25 16:32:14 +0100

    New release 2.2

--- _define.php
+++ _define.php
@@ -15,9 +15,9 @@ $this->registerModule(
     'Template Helper',
     'Template Helper methods',
     'Franck Paul',
-    '2.1',
+    '2.2',
     [
-        'date'        => '2025-03-15T23:44:00+02.1',
+        'date'        => '2025-03-15T23:44:00+02.2',
         'requires'    => [['core', '2.36']],
         'type'        => 'plugin',
         'permissions' => 'My',
